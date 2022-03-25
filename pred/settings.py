18af20fac00fd6587409f859aa5d2bcc2f5dcf14
zhenyu1311 <101508184+zhenyu1311@users.noreply.github.com>
2022-03-25 20:15:56 +0800

    Update settings.py

--- pred/settings.py
+++ pred/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'y8d*&k0jv4c*zu^ykqz$=yyv@(lcmz495uj^()hthjs=x&&g0y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.herokuapp.com']
 
 
 # Application definition
